c145c82d674f816b3c8db05478f82040a2d99183
Mahadi Hasan <mahadihk@outlook.com>
2021-02-19 20:12:16 +0600

    Auth implemented, profile migration created, Landing page modified

---
+++ database/migrations/2021_02_19_123954_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->tinyinteger('gender');
+            $table->string('name_of_father', 50);
+            $table->string('picture');
+            $table->date('date_of_birth');
+            $table->text('present_address');
+            $table->text('permanent_address');
+            $table->string('mobile', 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ProfileController resources controller Route
+Route::resource('profile', ProfileController::class);
